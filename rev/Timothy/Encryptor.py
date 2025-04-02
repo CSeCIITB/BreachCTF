356a14aa338345b95838b677b644ee8f3e92a4cf
Sam-MARTis <samanthmartis@gmail.com>
2025-04-02 23:51:46 +0530

    Added CA

---
+++ rev/Timothy/Encryptor.py
@@ -0,0 +1,87 @@
+
+def pretty(arr):
+    return ''.join(map(str, arr))
+
+def convertBinStrToStr(binStr):
+
+    bytesStr = [binStr[i:i+8] for i in range(0, len(binStr), 8)]
+    outStr = ''.join([chr(int(byte, 2)) for byte in bytesStr])
+    return outStr
+
+    
+
+class Encryptor:
+    def __init__(self, rules):
+        self.oldStates = []
+        self.currentStates = []
+        self.currentStateBuffer = []
+        # self.cellCount = cellCount
+        self.rulesToFlip = rules
+        self.cellCount = 0
+        
+        pass
+    def initialize(self, prevState: str, stringToEncrypt):
+        ivBinStr = ''.join(format(ord(i), '08b') for i in prevState)
+        binStr = ''.join(format(ord(i), '08b') for i in stringToEncrypt)
+
+        
+        self.cellCount = max(len(ivBinStr), len(binStr))
+        self.currentStates = [0 for _ in range(self.cellCount)]
+        self.oldStates = self.currentStates[:]
+        self.currentStateBuffer = self.currentStates[:]
+        
+        
+      
+        ivBinStr += '0'*(self.cellCount-len(ivBinStr))
+        binStr += '0'*(self.cellCount-len(binStr))
+        print(len(binStr))
+        for i in range(self.cellCount):
+            self.oldStates[i] = int(ivBinStr[i])
+            self.currentStates[i] = int(binStr[i])
+            self.currentStateBuffer[i] = self.currentStates[i]
+        
+        
+    def calcNextState(self):
+        self.cellCount = len(self.currentStates)
+        radius =  (len(self.rulesToFlip[0])-1)//2
+        for i in range(self.cellCount):
+            cellStates = ""
+            for j in range(-radius, radius+1):
+                idx = (i+j)%self.cellCount
+                cellStates += str(self.currentStates[idx])
+
+                    
+
+            if cellStates in self.rulesToFlip:
+                self.currentStateBuffer[i] = 1-int(self.oldStates[i])
+            else:
+                self.currentStateBuffer[i] = self.oldStates[i]
+    
+    def syncBuffer(self):
+        for i in range(self.cellCount):
+            self.oldStates[i] = self.currentStates[i]
+        for i in range(self.cellCount):
+            self.currentStates[i] = self.currentStateBuffer[i]
+            
+    def iterate(self, iterations):
+        for i in range(iterations):
+            self.calcNextState()
+            self.syncBuffer()
+        return self.oldStates, self.currentStates
+    
+    def getOutput(self, type=0):
+        if(type==1):
+            return ''.join(map(str, self.currentStateBuffer))
+        return  convertBinStrToStr(''.join(map(str, self.currentStates)))
+    def getOldOutput(self, type=0):
+        if(type==1):
+            return ''.join(map(str, self.oldStates))
+        return  convertBinStrToStr(''.join(map(str, self.oldStates)))
+        
+            
+            
+            
+                
+                
+            
+
---
+++ rev/Timothy/main.py
@@ -0,0 +1,128 @@
+from Encryptor import Encryptor, convertBinStrToStr
+from SECRET import rules, FLAG
+import socket
+from jokes import jokes
+import random
+
+
+Timothy = Encryptor(rules)
+Anthony = Encryptor(rules)
+
+IV = "h9eunjhvbfhixmjvth7ewjswjhf9ko"  # Beep Bop Why Not
+Timothy.initialize(IV, FLAG)
+
+old, current = Timothy.iterate(1000)
+running = 1
+while running:
+    print(
+        "Welcome! What would you like to do?\n1: Encrypt something?\n2: Try fruitlessly to break the awesome encryption?\n3: Listen to a joke?\n4: Switch to binary input/output\n5: Switch to conentional input/output\n6: Exit\n"
+    )
+
+    data = input()
+    if not data:
+        print("Idk what u entered 1. BOOOOO^2. Goodbye!\n")
+        break
+    if data.strip() == "1":
+        print("Enter something to encrypt:\n")
+        data = input()
+        if not data:
+            print("Idk what u entered 2. BOOOOO. Goodbye!\n")
+            running = 0
+            break
+        try:
+            if communicationType == 1:
+                try:
+
+                    val = convertBinStrToStr(data[:-1])
+                except Exception as e:
+                    print("Bruh, I need a binary representation. Goodbye!\n")
+                    running = 0
+
+            Anthony.initialize(IV, val)
+            iterating = 1
+            while iterating:
+                print(
+                    "How many times do u wanna iterate on it? (cumulative)\n"
+                )
+                data = input()
+                try:
+                    num = int(data.strip())
+                except Exception as e:
+                    print("Bruh, I need a number. Goodbye!\n")
+                    running = 0
+                    break
+                if num > 10000:
+                    print(
+                        "Are u trying to kill me? Send a small number! I won't tolerate this. Goodbye\n"
+                    )
+                    running = 0
+                    break
+                Timothy.iterate(num)
+                out = Timothy.getOutput(type=communicationType)
+                print("Encrypted output:\n\n\n")
+                print(out + "\n\n\n")
+                print(
+                    "\nContinue encryption iteration? (y/n) \n"
+                )
+                data =input()
+                if data.strip() not in ["y", "n"]:
+                    print(
+                        "Idk what u entered 3. BOOOOO. Goodbye!\n"
+                    )
+                    running = 0
+                    break
+
+                if data.strip() == "y":
+                    continue
+                else:
+                    break
+
+        except Exception as e:
+            print(
+                "Something went wrong! I aint dealing with that. Goodbye!\n"
+            )
+            running=0
+            break
+    elif data.strip() == "2":
+        print("Try to break the encryption:\n\n\n")
+        print(
+            Timothy.getOutput(type=communicationType)
+            + "\n\n\n"
+        )
+        print(
+            "To make it easier, I will give u the previous state as well!:\n\n\n"
+        )
+        print(
+            Timothy.getOldOutput(type=communicationType)
+            + "\n\n\n"
+        )
+
+    elif data.strip() == "3":
+        joke = random.choice(jokes)
+        print("Here's a joke for you:\n")
+        print(joke + "\n\n")
+    elif data.strip() == "4":
+        communicationType = 1
+        print("Switched to binary\n\n")
+    elif data.strip() == "5":
+        communicationType = 0
+        print("Switched to conventional\n\n")
+    elif data.strip() == "6":
+        print("Fair winds, Traveler\n")
+        running=0
+
+        break
+    else:
+        print("Idk what u entered 4. BOOOOO. Goodbye!\n")
+        break
+
+
+
+# if __name__ == "__main__":
+#     while True:
+#         try:
+#             start_server()
+#         except Exception as e:
+#             print(e)
+#             print("Restarting server...")
+#     # start_server()
