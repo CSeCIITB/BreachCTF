0aa0505d22de27af3a039dea42556c87a78eaad6
Mohana Evuri <148174808+rennaMAhcuS@users.noreply.github.com>
2025-04-19 23:05:46 +0530

    Updated `solve.py` (#1)

--- misc/pyshell/solution/solve.py
+++ misc/pyshell/solution/solve.py
@@ -1,7 +1,9 @@
 from pwn import *
 import hashlib
 import time
+import sys
 
+context.log_level='debug'
 # Precompute hash_prefixes
 hash_map = {}
 for i in range(1, 10001):
@@ -10,13 +12,19 @@ for i in range(1, 10001):
 
 PORT = 1340
 
-io = remote(HOST, PORT)
+io = remote('challs.breachers.in', 1340)
 
 # Wait for prompt, send git commit
+commit_time = str(int(time.time()))  # Estimate commit time
 io.sendlineafter("$", "git commit")
-t = str(int(time.time()))  # Estimate commit time
 # Capture the commit timestamp
 io.recvuntil("Committed.")
+commit_time_after = str(int(time.time()))
+
+# This is to ensure that the `time.time()` records the correct time.
+# You can also brute-force all the timestamps between `commit_time` and `commit_time_after`.
+if commit_time != commit_time_after:
+	sys.exit()
 
 # Brute-force the commit hash
 log.info(f"Brute-forcing commit ID for timestamp: {commit_time}")
@@ -26,9 +34,9 @@ for hash_prefix, rand_val in hash_map.items():
     io.sendline(f"git snapshot {commit_id}")
     response = io.recvline(timeout=1)
 
-    if b"Success" in response:  # Adjust this check based on expected output
+    if b"Breach" in response:  # Adjust this check based on expected output
         log.success(f"Found valid commit ID: {commit_id}")
         break
 
-# Keep interactive session open
+# Keep the interactive session open
 io.interactive()
